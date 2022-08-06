5d9a53356c601f110da623e2acd03b931ce3508b
Gaurav Sapkal <gauravsapkal1997@gmail.com>
2022-08-06 09:44:56 +0530

    job list page with different sorting filters

---
+++ job_List_Page.js
@@ -0,0 +1,338 @@
+
+var objArr = [
+    {
+        post : "Fullstack Developer",
+        company : "ZOLOSTAYS",
+        exp : "1 year",
+        salary : "₹ 5,00,000 - 10,00,000 P.A.",
+        loc : "Banglore",
+        skill : "Java",
+        details : "Vacancy Full stack Developer Candidates serving more than 1 month notice period can apply",
+        linkofpage : "https://www.naukri.com"
+    },
+    {
+        post : "Backend Developer",
+        company : "Amazon",
+        exp : "3 years",
+        salary : "₹ 8,00,000 - 15,00,000 P.A.",
+        loc : "Mumbai",
+        skill : "Java . SQL . Automation",
+        details : "Vacancy Backend Developer Candidates serving more than 1 week notice period can apply",
+        linkofpage : "https://www.naukri.com/browse-jobs"
+    },
+    {
+        post : "Frontend Developer",
+        company : "TCS",
+        exp : "Fresher",
+        salary : "₹ 3,00,000 - 5,00,000 P.A.",
+        loc : "Pune",
+        skill : "Automation",
+        details : "Vacancy Frontend Developer Candidates serving more than 1 week notice period can apply",
+        linkofpage : "https://www.naukri.com"
+    },
+
+    {
+        post : "Associate Software Engineer",
+        company : "Precisely",
+        exp : "4 years",
+        salary : "₹ Not disclosed",
+        loc : "Noida",
+        skill : "Front end . Linux . Javascript",
+        details : "Bachelor s degree in Computer Science or related field, or equivalent experience.",
+        linkofpage : "https://www.naukri.com"
+    },
+
+    {
+        post : "Lead Full-Stack Developer",
+        company : "BNY Mellon",
+        exp : "6 years",
+        salary : "₹ Not disclosed",
+        loc : "Pune",
+        skill : "Computer science . Manager Quality Assurance . Front end . Coding",
+        details : "Bachelors degree in computer science engineering or a related discipline, or equivalent",
+        linkofpage : "https://www.naukri.com"
+    },
+
+    {
+        post : "Functional Test Planning Quality Engineer (Tester)",
+        company : "Accenture",
+        exp : "2 years",
+        salary : "₹ 3,00,000 - 5,00,000 P.A.",
+        loc : "Pune",
+        skill : "Automation . Consulting . Test execution . Agile methodology",
+        details : "Additional Information : Resource need to work in ME shift viz 11 am to 8 pm Technical",
+        linkofpage : "https://www.naukri.com"
+    },
+
+    {
+        post : "Catia Product Design Engineer",
+        company : "Peoplefy Infosolutions Pvt. Ltd.",
+        exp : "Fresher",
+        salary : "₹ Not disclosed",
+        loc : "Delhi",
+        skill : "presentation . PFMEA . Communication . DFMEA . GD&T . CATIA",
+        details : "Taking up ownership and accountability to drive the project with minimum supervision",
+        linkofpage : "https://www.naukri.com"
+    },
+
+    {
+        post : "Software Quality Assurance Engineer",
+        company : "Nuance India",
+        exp : "Fresher",
+        salary : "₹ 3,00,000 - 5,00,000 P.A.",
+        loc : "Pune",
+        skill : "Cloud Native testing . REST API . Core Java . Git . Containerization",
+        details : "This individual will join the Digital product QA team in Enterprise R&D. This team owns",
+        linkofpage : "https://www.naukri.com"
+    },
+    
+
+]
+
+localStorage.setItem("masterJobList", JSON.stringify(objArr));
+var objArr = JSON.parse(localStorage.getItem("masterJobList")) || [];
+
+// Registered jobs by employer comes here
+
+// var jobsPosted = JSON.parse(localStorage.getItem("registerJob")) || [];
+
+// jobsPosted.map(function(elem){
+//     objArr.unshift(elem);
+// })
+
+// Registered jobs by employer comes here
+
+
+    displayJobList(objArr);
+
+    function displayJobList(data){
+        document.getElementById("middleJobList").innerText = "";
+
+        var upperFilter = document.createElement("div");
+
+        upperFilter.innerHTML = '<p><i class="fa-solid fa-filter"></i> Sort by:</p> <select id="sortJobs" onchange="freshFilter()"> <option value = "">Select experience</option> <option value = 1>All</option> <option value= 2>Fresher</option> <option value = 3>Experienced</option> </select>'
+        document.getElementById("middleJobList").append(upperFilter);
+
+        data.map(function(ele){
+
+        var maindiv = document.createElement("div");        ////
+
+        var anchor = document.createElement("a");
+
+        anchor.href = ele.linkofpage;  //////////////////////
+
+        anchor.setAttribute("class", "anchorForDetailPage");
+        anchor.setAttribute("target", "_blank");
+        var h2 = document.createElement("h2");              ////
+        h2.innerText = ele.post;
+        anchor.append(h2);
+        var pstar = document.createElement("p");
+
+        pstar.innerHTML = ele.company + " . 4.1" + ' <span id="starMark">★</span>';  ////
+
+        var subdiv = document.createElement("div");      
+
+        var p1 = document.createElement("p");
+        p1.innerHTML = '<i class="fa-solid fa-briefcase"></i> ' + ele.exp;
+
+        var p2 = document.createElement("p");
+        p2.innerText = ele.salary;
+
+        var p3 = document.createElement("p");
+        p3.innerHTML = '<i class="fa-solid fa-location-dot"></i> ' + ele.loc;
+
+        subdiv.append(p1, p2, p3);                       //////
+
+
+
+        var pdetails = document.createElement("p");        //////
+        pdetails.innerHTML = '<i class="fa-solid fa-address-card"></i> ' + ele.details;
+
+
+        var pskill = document.createElement("p");                ///////
+        pskill.innerHTML = ele.skill;
+
+        var btn = document.createElement("button");                 ///////////
+        btn.innerText = "✰ Save";
+        btn.setAttribute("id", "saveTheJobButton");
+
+        maindiv.append(anchor, pstar, subdiv, pdetails, pskill, btn);
+
+        document.getElementById("middleJobList").append(maindiv);
+      });
+    }
+
+
+
+// left side filter JS------------------------------------------------------------------------------
+
+// filteration in check box
+document.getElementById("comp1").addEventListener("change", filt1);
+document.getElementById("comp2").addEventListener("change", filt2);
+// document.getElementById("comp3").addEventListener("change", filt3);
+document.getElementById("comp4").addEventListener("change", filt4);
+document.getElementById("comp5").addEventListener("change", filt5);
+
+document.getElementById("loc1").addEventListener("change", filtloc1);
+document.getElementById("loc2").addEventListener("change", filtloc2);
+document.getElementById("loc3").addEventListener("change", filtloc3);
+document.getElementById("loc4").addEventListener("change", filtloc4);
+
+function freshFilter(){
+    var chk = document.getElementById("sortJobs").value;
+    var filtereddata = objArr;
+
+    if(chk == 2){
+        filtereddata = objArr.filter(function(elem){
+            return elem.exp == "Fresher";
+        })
+    }
+
+     if(chk == 3){
+        filtereddata = objArr.filter(function(elem){
+            return elem.exp != "Fresher";
+        })
+    }
+
+    displayJobList(filtereddata);
+}
+
+
+
+function filt1(){
+    if(document.getElementById("comp1").checked==true){
+        var ans = document.getElementById("comp1").value;
+
+        var filteredArr = objArr.filter(function(elem){
+           return ans == elem.company;
+        });
+        displayJobList(filteredArr);
+    }else{
+        // display whole data
+        displayJobList(objArr);
+    }
+}
+
+
+function filt2(){
+    if(document.getElementById("comp2").checked==true){
+        var ans = document.getElementById("comp2").value;
+
+        var filteredArr = objArr.filter(function(elem){
+           return ans == elem.company;
+        });
+        displayJobList(filteredArr);
+    }else{
+        // display whole data
+        displayJobList(objArr);
+    }
+}
+
+
+// function filt3(){
+//     if(document.getElementById("comp3").checked==true){
+//         var ans = document.getElementById("comp3").value;
+
+//         var filteredArr = objArr.filter(function(elem){
+//            return ans == elem.company;
+//         });
+//         displayJobList(filteredArr);
+//     }else{
+//         // display whole data
+//         displayJobList(objArr);
+//     }
+// }
+
+
+
+
+
+// left side filter JS------------------------------------------------------------------------------
+
+// filteration in check box
+
+function filt4(){
+    if(document.getElementById("comp4").checked==true){
+        var ans = document.getElementById("comp4").value;
+
+        var filteredArr = objArr.filter(function(elem){
+           return ans == elem.company;
+        });
+        displayJobList(filteredArr);
+    }else{
+        // display whole data
+        displayJobList(objArr);
+    }
+}
+
+function filt5(){
+    if(document.getElementById("comp5").checked==true){
+        var ans = document.getElementById("comp5").value;
+
+        var filteredArr = objArr.filter(function(elem){
+           return ans == elem.company;
+        });
+        displayJobList(filteredArr);
+    }else{
+        // display whole data
+        displayJobList(objArr);
+    }
+}
+
+function filtloc1(){
+    if(document.getElementById("loc1").checked==true){
+        var ans = document.getElementById("loc1").value;
+
+        var filteredArr = objArr.filter(function(elem){
+           return ans == elem.loc;
+        });
+        displayJobList(filteredArr);
+    }else{
+        // display whole data
+        displayJobList(objArr);
+    }
+}
+
+function filtloc2(){
+    if(document.getElementById("loc2").checked==true){
+        var ans = document.getElementById("loc2").value;
+
+        var filteredArr = objArr.filter(function(elem){
+           return ans == elem.loc;
+        });
+        displayJobList(filteredArr);
+    }else{
+        // display whole data
+        displayJobList(objArr);
+    }
+}
+
+function filtloc3(){
+    if(document.getElementById("loc3").checked==true){
+        var ans = document.getElementById("loc3").value;
+
+        var filteredArr = objArr.filter(function(elem){
+           return ans == elem.loc;
+        });
+        displayJobList(filteredArr);
+    }else{
+        // display whole data
+        displayJobList(objArr);
+    }
+}
+
+function filtloc4(){
+    if(document.getElementById("loc4").checked==true){
+        var ans = document.getElementById("loc4").value;
+
+        var filteredArr = objArr.filter(function(elem){
+           return ans == elem.loc;
+        });
+        displayJobList(filteredArr);
+    }else{
+        // display whole data
+        displayJobList(objArr);
+    }
+}
+
+
